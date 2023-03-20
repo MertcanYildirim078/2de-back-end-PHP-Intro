3e5b6b23d7ac910882c60f2e0f1b87a14b7c41c2
MertcanYildirim078 <99068314@mydavinci.nl>
2023-03-20 16:01:50 +0100

    Goedemorgen eindopdracht af

--- Eindopdracht Goedemorgen/index.php
+++ Eindopdracht Goedemorgen/index.php
@@ -7,11 +7,40 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Hallo dag!</title>
 
+</head>
+<body>
+
+<?php
+
+$timezone = date_default_timezone_set("Europe/Amsterdam");
+$date = date("H:i");
+
+if ($date >= "06:00" && $date <= "12:00") {
+    echo "<h1 id = 'goedeDag'>Goede morgen!</h1>";
+    $bgImage = "morning.png";
+}
+    else if ($date >= "12:00" && $date < "18:00") {
+        echo "<h1 id = 'goedeDag'>Goede middag!</h1>";
+        $bgImage = "afternoon.png";
+    }
+        else if ($date >= "18:00" && $date <= "23:59") {
+            echo "<h1 id = 'goedeDag'>Goede avond!</h1>";
+            $bgImage = "evening.png";
+        }
+            else if ($date >= "00:00" && $date <= "06:00") {
+                echo "<h1 id = 'goedeDag'>Goede nacht!</h1>";
+                $bgImage = "night.png";
+            }
+
+echo "<h1 id = 'tijd'>Het is nu $date</h1>";
+
+?>
+
 <style>
 
     body{
-        background-color: turquoise;
         padding-top: 150px;
+        background-image: url(<?= $bgImage ?>);
     }
 
     #tijd, #goedeDag {
@@ -21,31 +50,6 @@
 
 
 </style>
-
-</head>
-<body>
-
-<?php
-
-$timezone = date_default_timezone_set("Europe/Madrid");
-$date = date("H:i");
-
-if ($date >= "06:00" && $date <= "12:00") {
-    echo "<h1 id = 'goedeDag'>Goede morgen!</h1>";
-}
-    else if ($date >= "12:00" && $date <= "18:00") {
-        echo "<h1 id = 'goedeDag'>Goede middag!</h1>";
-    }
-        else if ($date >= "18:00" && $date <= "00:00") {
-            echo "<h1 id = 'goedeDag'>Goede avond!</h1>";
-        }
-            else if ($date >= "00:00" && $date <= "06:00") {
-                echo "<h1 id = 'goedeDag'>Goede nacht!</h1>";
-            }
-
-echo "<h1 id = 'tijd'>Het is nu $date</h1>";
-
-?>
 
 </body>
 </html>
